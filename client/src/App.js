35e0afebc4fa22114eb01bf2ccab3f200030f7a4
Barathaner <karl-jahnel@web.de>
2022-10-23 20:58:13 +0200

    added frontend backend

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react'
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import Accordion from './components/Accordion';
+import moduleList from "./data.json";
+function App() {
+
+    const [data, setData] = useState([{}])
+    const [names, setNames] = useState([]);
+    useEffect(() =>{
+        fetch("/members").then(
+            res => res.json()
+        ).then(
+            data =>{
+                setData(data)
+                console.log(data)
+            }
+        )
+        },[])
+
+    function handleaddMods(evt) {
+        setNames(current => [...current, evt]);
+    }
+
+    return (
+        <div className="App">
+            <h2 className="welcomeText">Such deine Module zusammen!</h2>
+            <SearchBar  addMods={handleaddMods} placeholder="Enter a Subject Name..." data={moduleList} />
+            <div className="accordion">
+                {names.map(( name) => (
+                    <Accordion title={name} content="sdwasdf" />
+                ))}
+            </div>
+        </div>
+    );
+/*        <div>
+            {(typeof data.members === 'undefined')? (
+                <p>Loading...</p>
+            ):(
+                data.members.map((member,i) => (
+                    <p key={i}>{member} </p>
+                ))
+            )}
+        </div>*/
+}
+
+export default App
