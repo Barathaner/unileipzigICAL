d53ef324734daccb9de715ddc7d29037086f7efd
Barathaner <karl-jahnel@web.de>
2022-11-02 16:51:51 +0100

    komischer rand weg

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import Accordion from './components/Accordion';
+
 function App() {
 
     const [data, setData] = useState([{}])
@@ -9,35 +10,33 @@ function App() {
     const [chosen, setChosen] = useState([]);
 
 
-
-    useEffect(() =>{
+    useEffect(() => {
         fetch("/members").then(
             res => res.json()
         ).then(
-            data =>{
+            data => {
                 setData(data.members)
             }
         )
-        },[])
+    }, [])
 
 
+    function postdata(nameslol) {
 
-        function postdata(nameslol){
-
-            fetch('/add', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(nameslol)
+        fetch('/add', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(nameslol)
+        })
+            .then((response) => {
+                console.log("response", response)
+                return response.json()
             })
-                .then((response) => {
-                    console.log("response", response)
-                    return response.json()
-                })
-                .then((data) => setChosen(data))
-                .catch(err => {
-                    console.log("err" , err);
-                });
-        }
+            .then((data) => setChosen(data))
+            .catch(err => {
+                console.log("err", err);
+            });
+    }
 
     function handleaddMods(evt) {
         console.log("evt", [...names, evt])
@@ -45,12 +44,46 @@ function App() {
         postdata([...names, evt])
     }
 
+
+
+
+
+    function handleClickCAL(){
+            fetch('/ics', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/ics',
+    },
+  })
+  .then((response) => response.blob())
+  .then((blob) => {
+    // Create blob link to download
+    const url = window.URL.createObjectURL(
+      new Blob([blob]),
+    );
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute(
+      'download',
+      `cal.ics`,
+    );
+
+    // Append to html link element page
+    document.body.appendChild(link);
+
+    // Start download
+    link.click();
+
+    // Clean up and remove the link
+    link.parentNode.removeChild(link);
+  });
+    }
     return (
         <div className="App">
             <div className="row">
                 <div className="col-2"></div>
                 <div className="col-8">
-                    <SearchBar  addMods={handleaddMods} placeholder="Enter a Subject Name..." data={data} /></div>
+                    <SearchBar addMods={handleaddMods} placeholder="Enter a Subject Name..." data={data}/></div>
 
 
                 <div className="col-2"></div>
@@ -59,7 +92,8 @@ function App() {
             <div className="row mt-5">
                 <div className="col-2"></div>
                 <div className="col-8 d-flex justify-content-center">
-                    <button type="button" className="btn btn-success">GET ICAL</button></div>
+                    <button type="button" onClick={handleClickCAL} className="btn btn-success">GET ICAL</button>
+                </div>
 
 
                 <div className="col-2"></div>
@@ -70,7 +104,7 @@ function App() {
                 <div className="col-2"></div>
                 <div className="col-8">
                     <div className="accordion">
-                        {chosen !== undefined ? chosen.map((mods,index) => {
+                        {chosen !== undefined ? chosen.map((mods, index) => {
                             return <Accordion key={mods.id} title={mods.name} events={mods.events}/>
                         }) : null}
                     </div>
@@ -81,15 +115,15 @@ function App() {
 
         </div>
     );
-/*        <div>
-            {(typeof data.members === 'undefined')? (
-                <p>Loading...</p>
-            ):(
-                data.members.map((member,i) => (
-                    <p key={i}>{member} </p>
-                ))
-            )}
-        </div>*/
+    /*        <div>
+                {(typeof data.members === 'undefined')? (
+                    <p>Loading...</p>
+                ):(
+                    data.members.map((member,i) => (
+                        <p key={i}>{member} </p>
+                    ))
+                )}
+            </div>*/
 }
 
 export default App
