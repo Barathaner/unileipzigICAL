06bce1549be15bd67924b6d3a573d8c3445b08d1
karl <karl-jahnel@web.de>
2022-11-22 15:45:01 +0100

    added content header

--- backend/server.py
+++ backend/server.py
@@ -376,10 +376,8 @@ def daterange(date1, date2):
 def updateDatabase():
     time.sleep(5)
     with app.app_context():
-        if not database_exists(
-                f"postgresql://postgres:{os.environ['POSTGRES_PASSWORD']}@postgres:5432/{os.environ['POSTGRES_DB']}"):
-            db.create_all()
-            fillDatabase()
+        db.create_all()
+        fillDatabase()
         print("Updated database")
 
 
