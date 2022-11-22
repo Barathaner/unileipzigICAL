c3ed25559d641f8e6d5565fa91bb4273abc86cba
karl <karl-jahnel@web.de>
2022-11-22 15:34:16 +0100

    added content header

--- backend/config/gunicorn.py
+++ backend/config/gunicorn.py
@@ -1,6 +1,9 @@
+import multiprocessing
 import os
 
 bind = os.getenv('WEB_BIND','0.0.0.0:3000')
+workers = multiprocessing.cpu_count() *2
+threads = 3
 keyfile = os.getenv('PRIVKEY','/etc/letsencrypt/live/unikarlender.de/privkey.pem')
 certfile = os.getenv('CERT','/etc/letsencrypt/live/unikarlender.de/cert.pem')
 ca_certs = os.getenv('CHAIN','/etc/letsencrypt/live/unikarlender.de/chain.pem')
